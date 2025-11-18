bb5ff93a6b668653e0ccf936f55c0f4408d7fde0
lucasdDanielAlvesDeAguiar <lucasdalves0811@gmail.com>
2025-11-18 20:41:29 -0300

    add

--- operacoesmatematicas_test.c
+++ operacoesmatematicas_test.c
@@ -6,7 +6,7 @@
 int main(){
 
     assert(multiplicar(2, 3) == 6);
-    assert(multiplicar(2, 3) == -20);
+    assert(multiplicar(-4, 5) == -20);
 
         assert(dividir(10, 2) == 5);
         assert(dividir(-9, 3) == -3);
